7a834fa29c9797d39bc07219c9ae62b5e88f8332
Touhidul Shawan <touhidulshawan@gmail.com>
2021-02-19 15:22:29 +0600

    add icons on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useHistory, NavLink } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import ChatIcon from "./icons/ChatIcon";
+import SignoutIcon from "./icons/SignoutIcon";
 
 const NavBar = () => {
   const [errorMessage, setErrorMessage] = useState("");
@@ -25,12 +27,14 @@ const NavBar = () => {
       )}
       <nav className="bg-gray-50 shadow-md py-4 px-3 flex justify-end items-center lg:top-0 lg:left-0 lg:absolute lg:w-full lg:mb-5">
         <ul className="lg:mx-3 flex">
-          <li className="mr-3">
+          <li className="mr-4 flex items-center">
+            <ChatIcon />
             <NavLink className="text-blue-600" to="/chat">
               Chat
             </NavLink>
           </li>
-          <li>
+          <li className="flex items-center">
+            <SignoutIcon />
             <button
               className="cursor-pointer text-pink-600"
               onClick={handleLogout}
