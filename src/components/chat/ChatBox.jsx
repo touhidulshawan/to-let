1f2ff883fa8a2d32f34b32f8cadd7eb3c912b848
Touhidul Shawan <touhidulshawan@gmail.com>
2021-02-19 19:19:35 +0600

    minor edit

--- src/components/chat/ChatBox.jsx
+++ src/components/chat/ChatBox.jsx
@@ -34,6 +34,7 @@ const ChatBox = ({ messageRef }) => {
           className="p-3 w-10/12 md:w-5/12 bg-gray-200 md:border-2 md:border-gray-400 text-gray-700 focus:ring-2 ring-blue-400 md:rounded-md md:mr-3"
           onChange={(evt) => setMessageText(evt.target.value)}
           placeholder="type message here..."
+          required
         />
         <button
           className="bg-pink-600  text-pink-100 py-3 px-6 md:rounded-sm md:border-2 md:border-pink-600 uppercase tracking-wide md:w-1/12"
